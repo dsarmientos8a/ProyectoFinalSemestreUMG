1c6a6c011bc582dae4ffa6c34b1f4bd7e23b8b15
Lortiz <lortizh3@miumg.edu.gt>
2025-05-20 00:16:22 -0600

    Creacion del metodo para recuperar los precios del menu

--- CapaLogica/CLMenus.cs
+++ CapaLogica/CLMenus.cs
@@ -8,5 +8,29 @@ namespace CapaLogica
 {
     internal class CLMenus
     {
+        public int MtdTipoMenu(string Precio)
+        {
+            int PrecioMenu = 0;
+            switch(Precio)
+            {
+                case "Desayunos":
+                    PrecioMenu = 50;
+                    break;
+                case "Almuerzos":
+                    PrecioMenu = 100;
+                    break;
+                case "Cenas":
+                    PrecioMenu = 75;
+                    break;
+                case "Postres":
+                    PrecioMenu = 35;
+                    break;
+                case "Bebidas":
+                    PrecioMenu = 25;
+                    break;
+            }
+
+            return PrecioMenu;
+        }
     }
 }
